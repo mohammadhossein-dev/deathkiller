887d3561151aad0efcdb478e184c25d2fe4d3fe4
MohammadHossein <mohammadhossein.dev@gmail.com>
2022-02-17 19:42:26 +0330

    Patched some problems because of local variables

--- modules/user_input.py
+++ modules/user_input.py
@@ -1,6 +1,10 @@
 from colorama import Fore
 
+target_url = ""
+requests_count = 1
+
 def get_user_input():
+    global target_url
     target_url = input(Fore.GREEN+"[!] "+Fore.CYAN+"Please Enter the target URL => "+Fore.RED).strip()
     while target_url == "" or target_url == " " or target_url is None:
         target_url = input(Fore.GREEN + "[!] " + Fore.CYAN + "Please Enter the target URL => " + Fore.RED).strip()
@@ -11,6 +15,9 @@ def get_user_input():
         print(Fore.RED+"[*] "+Fore.LIGHTRED_EX+"Exiting the app ...")
         exit()
 
+    global requests_count
     requests_count = int(input(Fore.GREEN+"[!] "+Fore.CYAN+"How many requests do you want to send to "+Fore.LIGHTRED_EX+target_url+Fore.CYAN+" (>0) => "+Fore.RED).strip())
     while requests_count <= 0 or requests_count is None:
         requests_count = int(input(Fore.GREEN + "[!] " + Fore.CYAN + "How many requests do you want to send to " + Fore.LIGHTRED_EX + target_url + Fore.CYAN + " => " + Fore.RED).strip())
+
+
