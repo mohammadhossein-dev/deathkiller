10af4cea4d9644b9d75122573c83d16bfae31427
MohammadHossein <mohammadhossein.dev@gmail.com>
2022-02-17 20:09:20 +0330

    Added the ability to handle if any error has occurred while sending the request

--- modules/attack.py
+++ modules/attack.py
@@ -9,13 +9,23 @@ def attack():
 
     for i in range(1, user_input.requests_count+1):
 
-        req = requests.get(user_input.target_url)
+        try:
+            req = requests.get(user_input.target_url)
 
-        if req.status_code == 200:
-            print(Fore.GREEN+"[*] Request "+Fore.BLUE+str(i)+Fore.GREEN+" successfully sent to the target"+Fore.RESET)
-        elif req.status_code == 404:
-            print(Fore.LIGHTRED_EX+"[*] Request "+Fore.BLUE+str(i)+Fore.LIGHTRED_EX+" sent to the target, but got a 404 Not Found error, maybe the URL is wrong or deleted or moved to another one"+Fore.RESET)
-        elif str(req.status_code).startswith("3") :
-            print(Fore.YELLOW + "[*] Request " + Fore.BLUE + str(i) + Fore.YELLOW + " sent to the target, but got a 3xx code, maybe there was a redirection"+Fore.RESET)
-        else:
+            if req.status_code == 200:
+                print(Fore.GREEN + "[*] Request " + Fore.BLUE + str(i) + Fore.GREEN + " successfully sent to the target" + Fore.RESET)
+            elif req.status_code == 404:
+                print(Fore.LIGHTRED_EX + "[*] Request " + Fore.BLUE + str(i) + Fore.LIGHTRED_EX + " sent to the target, but got a 404 Not Found error, maybe the URL is wrong or deleted or moved to another one" + Fore.RESET)
+            elif str(req.status_code).startswith("3"):
+                print(Fore.YELLOW + "[*] Request " + Fore.BLUE + str(i) + Fore.YELLOW + " sent to the target, but got a 3xx code, maybe there was a redirection" + Fore.RESET)
+            else:
+                print(Fore.RED + "[*] The request couldn't be sent, there was a problem with your network or maybe the problem was from the server, if it repeated check everything then try again" + Fore.RESET)
+
+        except ConnectionResetError:
+            print(Fore.RED + "[*] The request couldn't be sent, The connection reset" + Fore.RESET)
+        except ConnectionRefusedError:
+            print(Fore.RED + "[*] The request couldn't be sent, Server refused the connection" + Fore.RESET)
+        except ConnectionAbortedError:
+            print(Fore.RED + "[*] The request couldn't be sent, Server aborted the connection" + Fore.RESET)
+        except ConnectionError:
             print(Fore.RED+"[*] The request couldn't be sent, there was a problem with your network or maybe the problem was from the server, if it repeated check everything then try again"+Fore.RESET)
